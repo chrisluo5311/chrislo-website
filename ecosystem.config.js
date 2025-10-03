7158352ec959de29c21184c630180f62218ec0b3
chrisluo5311 <chrislo5311@gmail.com>
2025-10-03 12:17:59 -0700

    Update deploy.yml and ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,5 @@
 module.exports = {
   apps: [
-    // dev
-    {
-      name: 'chrislo-web-dev',
-      script: './node_modules/.bin/vue-cli-service',
-      args: 'serve --port 3000 --host 0.0.0.0',
-      env: {
-        NODE_ENV: 'development',
-      },
-      watch: false,
-      autorestart: true,
-      max_memory_restart: '512M',
-      error_file: './logs/dev.err.log',
-      out_file: './logs/dev.out.log',
-    },
-
     // prod
     {
       name: 'chrislo-web',
@@ -22,7 +7,6 @@ module.exports = {
       args: ['-s', 'dist', '-l', '8080'],
       env: {
         NODE_ENV: 'production',
-        HOST: '0.0.0.0',
       },
       watch: false,
       autorestart: true,
