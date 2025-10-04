73cfeb2b80200ebccbc20f0bfca6db936ba5877d
chrisluo5311 <chrislo5311@gmail.com>
2025-10-04 11:49:46 -0700

    Update root in URL and Add hover style in navbar item

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
@@ -16,7 +16,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  // 預設值是 /(根路徑)
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
