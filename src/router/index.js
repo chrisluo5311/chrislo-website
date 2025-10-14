80f826319703b15b8e7303f64fdfc6967009bf69
chrisluo5311 <chrislo5311@gmail.com>
2025-10-14 01:47:59 -0700

    Add new project in HomeView and Add new page for arXiv project

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,12 @@ const routes = [
     component: () => import('@/views/DistributedShardingDB.vue'),
     meta: { title: 'Distributed Sharding DB' }
   },
+  {
+    path: '/projects/arxiv-copilot',
+    name: 'ArxivCopilot',
+    component: () => import('@/views/ArxivCopilot.vue'),
+    meta: { title: 'arXiv Copilot' }
+  },
   { 
     // temporary fallback
     path: '/:pathMatch(.*)*', 
